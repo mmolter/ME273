effd3403be59446baf03fe69c1ef4a9d66108685
M. Molter <mmolter@mail.bradley.edu>
2018-03-12 12:33:57 -0500

    Started working on ship projectile project.

--- Projects/Project 1/project_1.py
+++ Projects/Project 1/project_1.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def drag(v, D, rho, A):
     ''' Return drag force on a projectile subject to air resistance.
         
@@ -76,6 +77,86 @@ def air_density(y, rho0=1.2, c=6.5e-3, T0=300, alpha=2.5):
             (float):        air density at altitude (kg/m**3)
     '''
 
-    return rho0 * (1 - c * y / T0)**alpha
+    return (rhi0 * (1 - c * y / T0))**alpha
 
+def euler_method(acceleration, t0, tf, dt, x0=0, v0=0, modified=False, **kwargs):
+    ''' Returns t, x, v, and a arrays for Euler Method
+
+    Args:
+        acceleration (func):    acceleration function **
+
+        t0 (float):             initial time (s)
+        tf (float):             final time (s)
+        dt (float):             time step (s)
+        
+        x0 (np.array):          initial displacement (m)
+        v0 (np.array):          initial velocity (m/s)
+        **kwargs:               any additional parameters required for
+                                    acceleration function.
+
+    Returns:
+        np.float64 (np.array):  list of times (s)
+        np.float64 (np.array):  list positions (m)
+        np.float64 (np.array):  list velocities (m/s)
+        np.float64 (np.array):  list accelerations (m/s**2)
+    '''
+
+    t = np.arange(t0, tf, dt, dtype=np.float64)
+
+    points = t.size
+    dimensions, = x0.shape
+
+    x = np.empty([points, dimesnions])
+    v = np.empty([points, dimesnions])
+    a = np.empty([points, dimesnions])
+
+    x[0] = x0
+    v[0] = v0
+    a[0] = acceleration(x0, v0, **kwargs)
+
+    for i in range(1, points):
+        a[i] = acceleration(x[i-1], v[i-1], **kwargs)
+
+        if modified:
+            v[i] = v[i-1] + dt * a[i]
+            x[i] = x[i-1] + dt * v[i]
+        else:
+            x[i] = x[i-1] + dt * v[i-1]
+            v[i] = x[i-1] + dt * a[i-1]
+
+    return t, x, v, a
+
+def launch_vector(mag, incline, azimuth=False, radians=False):
+    ''' Return v0 vector for given scalar magnitude and incline above the horizon. 
+
+        Args:
+            magnitude (float):      magnitude of v0 (m/s)
+            incline (float):        angle above horizon (degrees)
+            azimuth (float):        angle in plane of horizon (degrees)
+            radians (bool):         option for using radian incline
+
+        Returns:
+            np.array (np.float64):  vector with launch velocity components
+    '''
+    
+    if not radians:
+        incline = incline * (180.0 / np.pi)
+        
+        if azimuth:
+            azimuth = azimuth * (180.0 / np.pi)
+    
+    if azimuth:
+        z = mag * np.sin(incline)
+        h = mag * np.cos(incline)
+
+        x = h * np.cos(azimuth)
+        y = h * np.sin(azimuth)
+
+        return np.asarray([x, y, z])
+
+    else:
+        x = mag * np.cos(incline)
+        y = mag * np.sin(incline)
+
+        return np.asarray([x, y])
 
